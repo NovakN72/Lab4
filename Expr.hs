fc3d8dc89f09ede600c4fa76c4a9eea266154381
narimaun <narimaun@chalmers.se>
2024-12-09 12:46:38 +0100

    f

--- Expr.hs
+++ Expr.hs
@@ -1,4 +1,6 @@
 import Parsing
+import Data.Char
+import Test.QuickCheck
 
 
 data Expr = Num Double
@@ -7,6 +9,7 @@ data Expr = Num Double
            | Mul Expr Expr
            | Sin Expr
            | Cos Expr
+           deriving Eq
 
 x :: Expr
 x = Var 
@@ -36,11 +39,21 @@ size (Mul ex1 ex2) = 1 + size ex1 + size ex2
 
 --B
 showExpr :: Expr -> String
-showExpr (Num n)       = "n"
+showExpr (Num n)       = show n
 showExpr Var           = "x"
-showExpr (Sin ex)      = "sin" ++ " " ++ showExpr ex
+showExpr (Sin ex)      = "sin" ++ case ex of 
+    Mul _ _    ->  " " ++ "(" ++ showExpr ex ++ ")"
+    Add _ _    ->  " " ++ "(" ++ showExpr ex ++ ")"
+    _          ->  " " ++ showExpr ex
+showExpr (Cos ex)      = "cos" ++ case ex of 
+    Mul _ _    ->  " " ++ "(" ++ showExpr ex ++ ")"
+    Add _ _    ->  " " ++ "(" ++ showExpr ex ++ ")"
+    _          ->  " " ++ showExpr ex
 showExpr (Add ex1 ex2) = showExpr ex1 ++ "+" ++ showExpr ex2
 showExpr (Mul ex1 ex2) = showFactor ex1 ++  "*" ++ showFactor ex2 
+
+instance Show Expr where 
+    show = showExpr
 
 showFactor :: Expr -> String
 showFactor (Add a b) = "("++showExpr (Add a b)++")"
@@ -55,9 +68,12 @@ eval (Cos ex) varValue = Prelude.cos (eval ex varValue)
 eval (Add ex1 ex2) varValue =  eval ex1 varValue + eval ex2 varValue
 eval (Mul ex1 ex2) varValue =  eval ex1 varValue * eval ex2 varValue
 
+ex :: Expr 
+ex = Sin (Add (Num 3.2) Var) 
+
 --D
 readExpr :: String -> Maybe Expr
-readExpr str = case parse expr str of
+readExpr str = case parse expr (removeSpaces str) of
     Just (result, "") -> Just result -- Successfully parsed
     _                 -> Nothing    -- Parsing failed or leftover input
 {- EBNF:
@@ -74,26 +90,79 @@ term = foldl1 Mul <$> chain factor (char '*')
 factor = 
     Num <$> number 
     <|> (char 'x' *> return Var)
-    <|> Sin <$> (parseSin *> factor)
-    <|> Cos <$> (parseCos *> factor)
+    <|> Sin <$> (char 's' *> char 'i' *> char 'n' *> factor)
+    <|> Cos <$> (char 'c' *> char 'o' *> char 's' *> factor)
     <|> (char '(' *> expr <* char ')')
 
 number :: Parser Double 
 number = readsP
 
-parseSin :: Parser String 
-parseSin = do 
-    _ <- char 's'
-    _ <- char 'i'
-    _ <- char 'n'
-    return "sin"
+removeSpaces :: String -> String
+removeSpaces = filter (not . isSpace)
 
-parseCos :: Parser String 
-parseCos = do 
-    _ <- char 'c'
-    _ <- char 'o'
-    _ <- char 's'
-    return "cos"
+assoc :: Expr -> Expr
+assoc (Add (Add e1 e2) e3) = assoc (Add e1 (Add e2 e3)) 
+assoc (Add e1 e2)          = Add (assoc e1) (assoc e2)
+assoc (Mul (Mul e1 e2) e3) = assoc (Mul e1 (Mul e2 e3)) 
+assoc (Mul e1 e2)          = Mul (assoc e1) (assoc e2)
+assoc (Sin ex)             = Sin (assoc ex)
+assoc (Cos ex)             = Cos (assoc ex)
+assoc e                    = e
+        
+
+--E
+prop_ShowReadExpr :: Expr -> Bool
+prop_ShowReadExpr ex = 
+    let showResult = showExpr ex
+        e          = readExpr showResult 
+    in case e of 
+         Nothing     -> False
+         Just result -> assoc result == assoc ex
+
+arbExpr :: Int -> Gen Expr
+arbExpr s = 
+    frequency [ (1, do n <- arbitrary
+                       return (Num n)),
+                (s, do a <- arbExpr s'
+                       b <- arbExpr s'
+                       return (Add a b)),
+                (s, do a <- arbExpr s'
+                       b <- arbExpr s'
+                       return (Mul a b)),
+                (1,return Var),
+                (s, do a <- arbExpr s'
+                       return (Sin a)),
+                (s, do a <- arbExpr s'
+                       return (Cos a))]
+    where s' = s `div` 2
+
+
+instance Arbitrary Expr where 
+  arbitrary = sized arbExpr
+
+--F
+simplify :: Expr -> Expr
+simplify (Add (Num a) (Num b)) = Num $ a + b
+simplify (Add Var (Num 0))   = Var
+simplify (Add (Num 0) Var)   = Var 
+simplify (Mul (Num a) (Num b)) = Num $ a * b
+simplify (Mul Var (Num 0))   = Num 0
+simplify (Mul (Num 0) Var)   = Num 0 
+simplify (Mul Var (Num 1))   = Var
+simplify (Mul (Num 1) Var)   = Var
+simplify e                   = e
+
+prop_simplify :: Expr -> Double -> Bool 
+prop_simplify ex n = eval ex n == eval (simplify ex) n 
+
+--G
+differentiate :: Expr -> Expr
+differentiate Var = Num 1 
+differentiate Num = 0 
+differentiate Add ex1 ex2 = ((differentiate ex1) * ex2) + ((differentiate ex2) * ex1)
+differentiate Sin ex = Cos ex 
+differentiate Cos ex = -Sin ex
+differentiate Mul ex1 ex2 = 
 
 
 
